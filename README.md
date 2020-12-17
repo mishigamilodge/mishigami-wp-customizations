84dc4bf56e60b67bf1f7a0397819e226b70553c4
Dave Miller <d.miller@nslodge.org>
2020-12-17 00:09:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Mishigami Lodge Custom Stuff 
+*Contributors: Dave Miller
+*Tags: order of the arrow, BSA, OA, boy scouts  
+*Requires at least: 5.6
+*Tested up to: 5.6
+*Stable tag: trunk  
+*License: GPLv2 or later  
+*License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Wordpress plugin to house custom stuff for the mishigami.org website
+
+## Description
+
+## Installation 
+
+
+## Changelog
+
---
+++ mishigami-custom.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Plugin Name: Mishigami Lodge Custom Stuff
+ * Plugin URI: https://github.com/mishigami/mishigami-wp-customizations
+ * Description: Wordpress plugin to house custom stuff for this website
+ * Version: 1.0
+ * Author: Dave Miller
+ * Author URI: http://twitter.com/justdavemiller
+ * Author Email: github@justdave.net
+ * */
+
+/*
+ * Copyright (C) 2020 David D. Miller
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// All of the meat is in the includes directory, to keep it organized.
+// Just pull it all in from here.
+//require_once("includes/chapter-map.php");

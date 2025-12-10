ecc2e1296a66b88688083c05d336826ac3f97d68
Dave Miller <d.miller@nslodge.org>
2025-12-09 21:12:25 -0500

    missed one for cache-busting

--- includes/unit-picker.php
+++ includes/unit-picker.php
@@ -29,7 +29,7 @@ function mish_unit_autocomplete_loader() {
         wp_enqueue_style('jquery-ui-css',
                     'https://ajax.googleapis.com/ajax/libs/jqueryui/' . $wp_scripts->registered['jquery-ui-core']->ver . '/themes/smoothness/jquery-ui.css',
                     false,
-                    false,
+                    $wp_scripts->registered['jquery-ui-core']->ver,
                     false);
         wp_enqueue_style( 'mish-unit-picker-css', plugins_url('css/unit-picker.css', dirname(__FILE__)), array(), filemtime(dirname(__FILE__) . '/../css/unit-picker.css'));
         wp_enqueue_script( 'mish-unit-picker', plugins_url('js/unit-picker.js', dirname(__FILE__)), array( 'jquery-ui-autocomplete' ), filemtime(dirname(__FILE__) . '/../js/unit-picker.js'), true );
